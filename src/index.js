04e08352b930fdbf508ff59a8e522d29d5921209
unknown <samotv82@gmail.com>
2020-09-29 23:36:06 +0300

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
+  if (typeof(array) !== 'undefined' && array.length > 0)
+  return Math.min.apply(null, array);
+  else
   return 0;
+
+  
+   
+  
+ 
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (typeof(array) !== 'undefined' && array.length > 0)
+  return Math.max.apply(null, array);
+    
+  else return 0;
+  
+  }
+
 
 exports.avg = function avg (array) {
-  return 0;
+ 
+  if (typeof(array) !== 'undefined' && array.length > 0
+  ){
+  var sum=0;
+ for(var i=0; i<array.length; i++){
+sum+=array[i];
+ }
+  var result=sum/array.length;
+  return result;
+}
+else
+    return 0;
+
 }
